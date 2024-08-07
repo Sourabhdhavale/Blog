24c06fb0cd2c93f843c0f9ca7c0dd42b5ee368a5
d4_83871_sourabh <sourabhsrh007@gmail.com>
2024-08-07 20:13:26 +0530

    url changed

--- app/src/config.jsx
+++ app/src/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    url: 'http://localhost:4000',
+    url: 'mysql://avnadmin:AVNS_-Vp8-4Gx90jNKvLJrEU@blogdb-sourabhsrh007-13fe.g.aivencloud.com:15503/defaultdb?ssl-mode=REQUIRED',
   }
   
   export default config
