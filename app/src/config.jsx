1b2a3a8c127fc5919a1fc93fb076ec95a63bd552
d4_83871_sourabh <sourabhsrh007@gmail.com>
2024-08-07 20:28:16 +0530

    Database name changed

--- app/src/config.jsx
+++ app/src/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    url: 'mysql://avnadmin:AVNS_-Vp8-4Gx90jNKvLJrEU@blogdb-sourabhsrh007-13fe.g.aivencloud.com:15503/defaultdb?ssl-mode=REQUIRED',
+    url: 'https://blog-ashy-sigma-92.vercel.app/',
   }
   
   export default config
