3067fdd3e3a4da03af943f2c21aeeb5ae32ffad0
d4_83871_sourabh <sourabhsrh007@gmail.com>
2024-08-08 09:28:39 +0530

    vercel.json configuration changed

--- server/db.js
+++ server/db.js
@@ -1,23 +1,46 @@
 const mysql = require('mysql2');
 
-// Create the connection pool. The pool-specific settings are the defaults
+// Create the connection pool using environment variables
 const pool = mysql.createPool({
-  host: 'blogmysqldb-sourabhsrh007-13fe.g.aivencloud.com',
-  user: 'avnadmin',
-  port:15503,
-  password: 'AVNS_-Vp8-4Gx90jNKvLJrEU',
-  database: 'blog_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  maxIdle: 10,
+  idleTimeout: 60000,
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
 });
 
-
 module.exports = {
   pool,
-}
+};
+
+
+// const mysql = require('mysql2');
+
+// // Create the connection pool. The pool-specific settings are the defaults
+// const pool = mysql.createPool({
+//   host: 'blog-db-sourabhsrh007-13fe.g.aivencloud.com',
+//   user: 'avnadmin',
+//   port:15503,
+//   password: 'AVNS_II_74P6nqsSE1SvR6hM',
+//   database: 'blog_db',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+//   queueLimit: 0,
+//   enableKeepAlive: true,
+//   keepAliveInitialDelay: 0,
+// });
+
+
+// module.exports = {
+//   pool,
+// }
 
