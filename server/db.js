d2140c64da3ce4c5b5ea871d8384577a84db6d25
d4_83871_sourabh <sourabhsrh007@gmail.com>
2024-08-08 09:32:09 +0530

    vercel.json configuration changed

--- server/db.js
+++ server/db.js
@@ -21,26 +21,3 @@ module.exports = {
 };
 
 
-// const mysql = require('mysql2');
-
-// // Create the connection pool. The pool-specific settings are the defaults
-// const pool = mysql.createPool({
-//   host: 'blog-db-sourabhsrh007-13fe.g.aivencloud.com',
-//   user: 'avnadmin',
-//   port:15503,
-//   password: 'AVNS_II_74P6nqsSE1SvR6hM',
-//   database: 'blog_db',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay: 0,
-// });
-
-
-// module.exports = {
-//   pool,
-// }
-
