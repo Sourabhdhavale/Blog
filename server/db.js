2cb70d7153614cc428c064d2adefd19260438a17
d4_83871_sourabh <sourabhsrh007@gmail.com>
2024-08-11 17:19:09 +0530

    vercel.json changed

--- server/db.js
+++ server/db.js
@@ -1,31 +1,12 @@
-// const mysql = require('mysql2');
-// require("dotenv").config();
-// // // Create the connection pool using environment variables
-// const pool = mysql.createPool({
-//   host: 'process.env.DB_HOST',
-//   user: process.env.DB_USER,
-//   port: process.env.DB_PORT,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-// module.exports = {
-//   pool,
-// };
-// // 
-
 const mysql = require('mysql2');
 require("dotenv").config();
 // // Create the connection pool using environment variables
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'D4-sourabh-83871',
-  port: '3306',
-  password: 'manager',
-  database: 'blog_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -35,6 +16,25 @@ module.exports = {
   pool,
 };
 // 
+
+// const mysql = require('mysql2');
+// require("dotenv").config();
+// // // Create the connection pool using environment variables
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'D4-sourabh-83871',
+//   port: '3306',
+//   password: 'manager',
+//   database: 'blog_db',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = {
+//   pool,
+// };
+// // 
 
 
 
